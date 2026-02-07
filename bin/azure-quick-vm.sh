69ae96f462f91f60fe9b66fa653fcd4e8a6da303
chengkai liang <ckm.liang@gmail.com>
2026-02-07 07:07:44 -0800

    docs: clarify quick vm delete prompt

--- bin/azure-quick-vm.sh
+++ bin/azure-quick-vm.sh
@@ -210,7 +210,7 @@ if [[ $KEEP_VM -eq 1 ]]; then
   exit 0
 fi
 
-read -r -p "Delete VM now? [Y/n] " answer
+read -r -p "Delete VM now? [Y/n] (use --keep to skip this prompt) " answer
 case "$answer" in
   n|N) warn "Leaving VM running. Re-run with --keep next time if you want to skip this prompt."; exit 0;;
   *) delete_vm_resources;;
